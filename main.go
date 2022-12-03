3807c583e395caecad62e66ceec5ce0b43129c68
Assane DIATTA <assanediatta96@gmail.com>
2022-12-03 13:38:34 +0000

    use then keyword go for run the function sendTicket in another thread

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 const conferenceTickets uint = 50
 
@@ -26,7 +29,7 @@ func main() {
 
 		if isValidName && isValidEmail && isValidTicketNumber {
 			bookTicket(userTickets, firstName, lastName, email)
-			sendTicket(userTickets, firstName, lastName, email)
+			go sendTicket(userTickets, firstName, lastName, email)
 
 			firstNames := getFirstNames()
 			fmt.Printf("The first name of bookings are: %v\n", firstNames)
@@ -108,6 +111,7 @@ func bookTicket(userTickets uint, firstName string, lastName string, email strin
 }
 
 func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)
 	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
 	fmt.Println("################")
 	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", ticket, email)
